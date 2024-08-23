0b8970f393b05a96065b841a59913c380a453a32
Tyrone21nc <78980129+Tyrone21nc@users.noreply.github.com>
2024-08-23 14:25:59 -0400

    Update WordleV2.py

--- WordleV2.py
+++ WordleV2.py
@@ -1,14 +1,1 @@
-"""
-Author: Romain Donfack Dzeinse
-Date: 7/2/24 about 8pm -> 8:17pm
-# Finished on 1/20/24 1:03 am
-Objective: Build better version of the WordleGame.py file
-"""
-import random
-import time
-
-# This will contain 5-letter words
-my_words = {1: "ARISE", 2: "ADIEU", 3: "AUDIO", 4: "SLATE", 5: "CRANE",
-            6: "RAISE", 7: "TRACE", 8: "SHARE", 9: "SOOTY", 10: "STARE"}
-
 
